967acaa8bbc4448db4a1358e89926b3e81ccdf2c
Rajat Bhatt <rajatbhatt999@gmail.com>
2023-06-03 16:08:00 +0530

    Fix eslint error

--- app/api/hello/route.ts
+++ app/api/hello/route.ts
@@ -1,4 +1,5 @@
-export default async function GET(request: Request) {
+// eslint-disable-next-line import/prefer-default-export
+export async function GET(request: Request) {
   if (request) {
     return new Response("Hello, Next.js!");
   }
